c3ec7103bf53f510d7da84026baa3cad4fbde70f
Ade Oke <oke.ade@gmail.com>
2020-11-02 23:43:31 +0000

    updated sqlalchemy connection and databases

--- environment.py
+++ environment.py
@@ -4,13 +4,14 @@ import sys
 
 class Environment:
     @classmethod
-    def database_key_name(cls, key_name):
+    def database_key_name(cls, dialect_key, key_name):
         path_to_file = os.path.realpath(__file__)
         directory = os.path.dirname(path_to_file)
         yaml_file = os.path.join(directory, 'database.yml')
+        
         with open(yaml_file) as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
-            return (data['database'][key_name])
+            return (data['database'][dialect_key][key_name])
     
 
 # print(Environment.database_key_name('password'))
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from environment import Environment
 from sqlalchemy import create_engine
 
 
-def connect():
-    username = Environment.database_key_name('username')
-    password = Environment.database_key_name('password')
-    db_name = Environment.database_key_name('db_name')
-    port = Environment.database_key_name('port')
-    connection_str = f'postgresql://{username}:{password}@localhost:{port}/{db_name}'
+def connect_to_postgres_db():
+    username = Environment.database_key_name('postgres', 'username')
+    password = Environment.database_key_name('postgres', 'password')
+    db_name = Environment.database_key_name('postgres', 'db_name')
+    port = Environment.database_key_name('postgres', 'port')
+    dialect = Environment.database_key_name('postgres', 'dialect')
+    host = Environment.database_key_name('postgres', 'host')
+    connection_str = f'{dialect}://{username}:{password}@{host}:{port}/{db_name}'
     engine = create_engine(connection_str)
     
     with engine.connect() as connection:
@@ -16,4 +18,30 @@ def connect():
         for row in result:
             print("role name is:", row['role_name'])
 
-connect()
+# connect_to_postgres_db()
+
+
+
+def connect_to_mysql_db():
+    username = Environment.database_key_name('mysql', 'username')
+    password = Environment.database_key_name('mysql', 'password')
+    db_name = Environment.database_key_name('mysql', 'db_name')
+    port = Environment.database_key_name('mysql', 'port')
+    dialect = Environment.database_key_name('mysql', 'dialect')
+    host = Environment.database_key_name('mysql', 'host')
+    connection_str = 'mysql+mysqlclient://jamie_vardy:Leicester@localhost/menagerie'
+    # connection_str = f'{dialect}://{username}:{password}@{host}/{db_name}'
+    # print(connection_str)
+    engine = create_engine(connection_str)
+
+    with engine.connect() as connection:
+        result = connection.execute("select * from tutorials_tbl")
+        
+        for row in result:
+            print("Tutorial title is:", row['tutorial_title'])
+
+
+connect_to_mysql_db()
+
+# 'mysql://scott:tiger@localhost/foo'
+
